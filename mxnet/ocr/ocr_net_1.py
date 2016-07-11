dc58fb31f5d1a5bf99f6ceace49b08fcb51521f7
FrankBlood <1062947208@qq.com>
2016-07-11 21:54:26 +0800

    ocr net one ocr completed

--- mxnet/ocr/ocr_net_1.py
+++ mxnet/ocr/ocr_net_1.py
@@ -1,25 +1,9 @@
 # -*-coding:utf8-*-
 import tensorflow as tf
-# import sys
 import numpy as np
 import cv2, random
 from io import BytesIO
 from captcha.image import ImageCaptcha
-
-# class OCRBatch(object):
-#     def __init__(self, data_names, data, label_names, label):
-#         self.data = data
-#         self.label = label
-#         self.data_names = data_names
-#         self.label_names = label_names
-
-    # @property
-    # def provide_data(self):
-    #     return [(n, x.shape) for n, x in zip(self.data_names, self.data)]
-
-    # @property
-    # def provide_label(self):
-    #     return [(n, x.shape) for n, x in zip(self.label_names, self.label)]
 
 def gen_rand():
     buf = ""
@@ -50,7 +34,7 @@ def gen_sample(captcha, width, height):
     img = cv2.imdecode(img, cv2.IMREAD_COLOR)
     img = cv2.resize(img, (width, height))
     img = np.multiply(img, 1/255.0)
-    img = img.transpose(2, 0, 1)
+    #img = img.transpose(2, 0, 1)
     return (num, img)
 
 class OCRIter(object):
@@ -60,8 +44,6 @@ class OCRIter(object):
         self.count = count
         self.height = height
         self.width = width
-        # self.provide_data = [('data', (batch_size, 3, height, width))]
-        # self.provide_label = [('softmax_label', (self.batch_size, num_label))]
         
     def __iter__(self):
         for k in range(self.count / self.batch_size):
@@ -69,16 +51,21 @@ class OCRIter(object):
             label = []
             for i in range(self.batch_size):
                 num, img = gen_sample(self.captcha, self.width, self.height)
-                img = np.transpose(img)
+                #img = np.transpose(img)
                 data.append(img)
                 label.append(get_label(num))
 
             data_all = np.array(data)
             label_all = np.array(label)
             label_batch = label_one_hot(label_all)
+            data_all = data_all.astype("float32")
+            label_batch = label_batch.astype("float64")
             data_names = ['data']
             label_names = ['softmax_label']
-            # data_batch = OCRBatch(data_names, data_all, label_names, label_all)
+            # print type(data_all), np.shape(data_all)
+            # print type(label_batch), np.shape(label_batch)
+            # print type(data_all[0, 0, 0, 0])
+            # print type(label_batch[0, 0])
             yield data_all, label_batch
             
     def reset(self):
@@ -96,74 +83,69 @@ def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 1, 1, 1], padding='SAME')
-
-# def Accuracy(label, pred):
-#     label = label.T.reshape((-1, ))
-#     hit = 0
-#     total = 0
-#     for i in range(pred.shape[0] / 4):
-#         ok = True
-#         for j in range(4):
-#             k = i * 4 + j
-#             if np.argmax(pred[k]) != int(label[k]):
-#                 ok = False
-#                 break
-#         if ok:
-#             hit += 1
-#         total += 1
-#     return 1.0 * hit / total
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 sess = tf.InteractiveSession()
 
-x = tf.placeholder(tf.float32, [None, 80 ,40, 3])
+x = tf.placeholder(tf.float32, [None, 40 ,40, 3])
 y_ = tf.placeholder("float", [None, 10])
 print '申请两个占位符'
+
 W_conv1 = weight_variable([5, 5, 3, 32])
 b_conv1 = bias_variable([32])
 h_conv1 = conv2d(x, W_conv1) + b_conv1
 h_pool1 = tf.nn.relu(max_pool_2x2(h_conv1))
 print '第一层卷积'
+
 W_conv2 = weight_variable([5, 5, 32, 32])
 b_conv2 = bias_variable([32])
 h_conv2 = conv2d(h_pool1, W_conv2) + b_conv2	
 h_pool2 = tf.nn.relu(max_pool_2x2(h_conv2))
 print '第二层卷积'
+
 W_conv3 = weight_variable([5, 5, 32, 32])
 b_conv3 = bias_variable([32])
 h_conv3 = conv2d(h_pool2, W_conv3) + b_conv3	
 h_pool3 = tf.nn.relu(max_pool_2x2(h_conv3))
 print '第三层卷积'
-W_fc1 = weight_variable([5*10*32, 256])
+
+W_fc1 = weight_variable([5*5*32, 256])
 b_fc1 = bias_variable([256])
-h_pool3_flat = tf.reshape(h_pool3, [-1, 5*10*32])
+h_pool3_flat = tf.reshape(h_pool3, [-1, 5*5*32])
 h_fc1 = tf.matmul(h_pool3_flat, W_fc1) + b_fc1
-print '全连接'
+print 'flatten'
+
 W_fc21 = weight_variable([256, 10])
 b_fc21 = bias_variable([10])
 h_fc21 = tf.nn.softmax(tf.matmul(h_fc1, W_fc21) + b_fc21)
 print '第一个数字'
 
 cross_entropy = -tf.reduce_sum(y_*tf.log(h_fc21))
-print '计算交叉熵'
+print 'cross_entropy'
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-print '梯度下降求解'
-correct_prediction = tf.equal(tf.argmax(h_fc21,1), tf.argmax(y_,1))
-print '计算正确个数'
+print 'train_step'
+# print tf.shape(y_conv)
+# print tf.shape(y_)
+correct_prediction = tf.equal(tf.argmax(h_fc21, 1), tf.argmax(y_, 1))
+print 'correct_prediction'
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-print '计算正确率'
+print 'accuracy'
 sess.run(tf.initialize_all_variables())
-print '运行sess'
+print 'run'
 
 batch_size = 50
 print '生成数据'
 i = 0
-for (batch_data, label_batch) in OCRIter(100000, batch_size, 40, 80):
-    print '训练模型'
+for (batch_data, label_batch) in OCRIter(100000, batch_size, 40, 40):
+    #print '训练模型'
     if i%100 == 0:
+        # print np.shape(batch_data), type(batch_data), type(batch_data[0, 0, 0, 0])
+        # print np.shape(label_batch), type(label_batch), type(label_batch[0, 0])
         train_accuracy = accuracy.eval(feed_dict={x:batch_data, y_:label_batch})
         print (i, train_accuracy)
-    train_step.run(feed_dict={x: batch_data, y_: batch_label})
+    train_step.run(feed_dict={x: batch_data, y_: label_batch})
     i = i + 1
-test_data, test_label = OCRIter(1000, 1000, 40, 80)
-print(accuracy.eval(feed_dict={x: test_data, y_: test_label}))
+
+for (test_data, test_label) in OCRIter(10000, 10000, 40, 40):
+    print '测试模型'
+    print(accuracy.eval(feed_dict={x: test_data, y_: test_label}))
